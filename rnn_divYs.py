252b68e8d1ce4fbbda33eed31b1238b046f0ab4a
Aldo Contreras <aldocontrego@gmail.com>
2018-04-20 12:40:34 +0200

    code update
    code update

--- rnn_divYs.py
+++ rnn_divYs.py
@@ -11,13 +11,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing sklearn tools for prepare data
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 # Importing the Keras libraries and packages
 #import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import LSTM
 from keras.layers import Dropout
 
 # Importing the dataset
@@ -31,35 +31,53 @@ def importData():
     return(X, y0, y1, y2, y3)
 
 def classifierAnn(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    y_train = sc.fit_transform(y_train)
+    # Feature Scaling
+    training_set = np.append(y, X, axis = 1)
+    sc = MinMaxScaler(feature_range = (0, 1))
+    training_set_scaled = sc.fit_transform(training_set)
+    # Creating a data structure with 60 timesteps and 1 output
+    X_train = []
+    y_train = []
+    timeSteps = 60
+    for elements in range(len(training_set_scaled[0])):
+        for i in range(timeSteps, len(training_set_scaled)):
+            X_train.append(training_set_scaled[i-60:i, elements])
+            y_train.append(training_set_scaled[i, elements])
     
-    classifier = Sequential()
-    classifier.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'tanh', input_dim = 10))
-    # classifier.add(Dropout(rate = 0.1))
-    classifier.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'tanh'))
-    # classifier.add(Dropout(rate = 0.1))
-    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'tanh')) #tanh linear
-    classifier.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['acc'])
-    classifier.fit(X_train, y_train, batch_size = 14, epochs = 100)
-    # Predicting the Test set results
-    y_pred = classifier.predict(X)
-    return(classifier, y_pred)
+    X_train, y_train = np.array(X_train), np.array(y_train)
+    # Reshaping
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    # Initialising the RNN
+    regressor = Sequential()
+    regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+    regressor.add(Dropout(0.2))
+    regressor.add(LSTM(units = 50, return_sequences = True))
+    regressor.add(Dropout(0.2))
+    regressor.add(LSTM(units = 50, return_sequences = True))
+    regressor.add(Dropout(0.2))
+    regressor.add(LSTM(units = 50))
+    regressor.add(Dropout(0.2))
+    regressor.add(Dense(units = 1))
+    regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+    regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+    
+    X_test = X_train
+    predicted_stock_price = regressor.predict(X_test)
+    predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+    return(regressor, predicted_stock_price)
 
 def plotData(y0, y1, y2, y3, y_pred0, y_pred1, y_pred2, y_pred3):
     fig, axs = plt.subplots(2, 2)
-    axs[0,0].set_title("Q1")
+    axs[0,0].set_title('Q1')
     axs[0,0].plot(y0, color = 'red', label = 'Test')
     axs[0,0].plot(y_pred0, color = 'blue', label = 'Predicted')
-    axs[1,0].set_title("Q2")
+    axs[1,0].set_title('Q2')
     axs[1,0].plot(y1, color = 'red', label = 'Test')
     axs[1,0].plot(y_pred1, color = 'blue', label = 'Predicted')
-    axs[0,1].set_title("Q3")
+    axs[0,1].set_title('Q3')
     axs[0,1].plot(y2, color = 'red', label = 'Test')
     axs[0,1].plot(y_pred2, color = 'blue', label = 'Predicted')
-    axs[1,1].set_title("Q4")
+    axs[1,1].set_title('Q4')
     axs[1,1].plot(y3, color = 'red', label = 'Test')
     axs[1,1].plot(y_pred3, color = 'blue', label = 'Predicted')
     plt.show()
@@ -67,17 +85,16 @@ def plotData(y0, y1, y2, y3, y_pred0, y_pred1, y_pred2, y_pred3):
 def main():
     [X, y0, y1, y2, y3] = importData()
     # y0
-    [classifier_y0, y_pred0] = classifierAnn(X, y0)
+    [regressor_y0, y_pred0] = classifierAnn(X, y0)
     # y1
-    [classifier_y1, y_pred1] = classifierAnn(X, y1)
+    [regressor_y1, y_pred1] = classifierAnn(X, y1)
     # y2
-    [classifier_y2, y_pred2] = classifierAnn(X, y2)
+    [regressor_y2, y_pred2] = classifierAnn(X, y2)
     # y3
-    [classifier_y3, y_pred3] = classifierAnn(X, y3)
+    [regressor_y3, y_pred3] = classifierAnn(X, y3)
     # Plot all
     plotData(y0, y1, y2, y3, y_pred0, y_pred1, y_pred2, y_pred3)
     
 if __name__ == "__main__":
     main()
-    
     
