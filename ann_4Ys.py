a228d1ce90723c0d7e5ccb345a32d29531fb9ebd
AldoContreGo <aldocontrego@gmail.com>
2018-05-11 12:48:37 +0200

    code improvements
    getDataset added

--- ann_4Ys.py
+++ ann_4Ys.py
@@ -20,70 +20,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 
-
-def usrDataFnc():
-    while True:
-        try:
-            prompt = ('[0]All [1]COMBO [2]CRUZEXT [3]CRUZINT [4]ELEFRONT [5]LATERAL [6]ROTZ \nTest: ')
-            kindOfData = int(input(prompt))
-            if (kindOfData >= 0) and (kindOfData <= 6):
-                break
-        except ValueError:
-            print('Select a number between 0 and 6')
-    while True:
-        try:
-            prompt = ('Kind\n [1] FlexS vs ShoulderAng\n [2] FlexS+IMUq vs ShoulderAng\n '
-                    '[3] IMUq vs ShoulderAng\n [4] PCA vs Shoulder\n [5] FlexS vs IMUq\n '
-                    '[6] PCA vs IMUq\n Select: ')
-            kindOfTest = int(input(prompt))
-            if (kindOfTest >= 1) and (kindOfTest <= 6):
-                break
-        except ValueError:
-            print('ValueError')
-    return(kindOfData, kindOfTest)
-
-def loadWSFunc(kindOfData):
-    # [0]All, [1]COMBO, [2]CRUZEXT, [3]CRUZINT, [4]ELEFRONT, [5]LATERAL, [6]ROTZ
-    if kindOfData == 1:
-        dataset = pd.read_csv('data/comboAll.csv')
-    elif kindOfData == 2:
-        dataset = pd.read_csv('data/cruzadoextAll.csv')
-    elif kindOfData == 3:
-        dataset = pd.read_csv('data/cruzadointAll.csv')
-    elif kindOfData == 4:
-        dataset = pd.read_csv('data/frontalAll.csv')
-    elif kindOfData == 5:
-        dataset = pd.read_csv('data/lateralAll.csv')
-    elif kindOfData == 6:
-        dataset = pd.read_csv('data/rotacionzAll.csv')
-    elif kindOfData == 0:
-        dataset = pd.read_csv('data/allData.csv')
-    return dataset
-
-def dataToRNN(dataset, kindOfTest):
-    # [1]FlexS vs ShoulderAng [2]FlexS+IMUq vs ShoulderAng [3]IMUq vs ShoulderAng
-    # [4]PCA vs ShoulderAng [5] FlexS vs IMUq [6] PCA vs IMUq
-    if kindOfTest == 1:     # reviewed
-        input = dataset.iloc[:, 20:35].values   # FlexS
-        target = dataset.iloc[:, 41:44].values  # ShoulderAng
-    elif kindOfTest == 2:
-        input1 = dataset.iloc[:, 20:35].values  # FlexS
-        input2 = dataset.iloc[:, 13:17].values  # IMUq
-        input = np.concatenate([input1, input2], axis=1)    # FlexS + IMUq
-        target = dataset.iloc[:, 41:44].values  # ShoulderAng
-    elif kindOfTest == 3:   # reviewed
-        input = dataset.iloc[:, 13:17].values   # IMUq
-        target = dataset.iloc[:, 41:44].values  # ShoulderAng
-    elif kindOfTest == 4:
-        input = dataset.iloc[:, 20:35].values   # TODO PCA analysis
-        target = dataset.iloc[:, 41:44].values  # ShoulderAng
-    elif kindOfTest == 5:   # reviewed
-        input = dataset.iloc[:, 20:35].values   # FlexS
-        target = dataset.iloc[:, 13:17].values  # IMUq
-    elif kindOfTest == 6:   # reviewed
-        input = dataset.iloc[:, 20:35].values   # TODO PCA analysis
-        target = dataset.iloc[:, 13:17].values   # IMUq
-    return (input, target)
+import usrInput
 
 # Data Preprocessing
 def importAndPrepare(X, y):
@@ -150,9 +87,7 @@ def plotData(y_pred, y):
     plt.show()
 
 def main():
-    [kindOfData, kindOfTest]=usrDataFnc()
-    dataset = loadWSFunc(kindOfData)
-    [input, target] = dataToRNN(dataset, kindOfTest)
+    [input, target] = usrInput.getDataset()
     X, y, X_train, X_test, y_train, y_test, sc = importAndPrepare(input, target)
     [classifier, y_pred] = createANN(X, X_train, X_test, y_train)
     # singlePrediction(classifier, sc, y_test, y_pred)
