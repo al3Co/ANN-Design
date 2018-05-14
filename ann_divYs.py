a2ffdd89f44aa085b01303590b3c09f5772a7b64
AldoContreGo <aldocontrego@gmail.com>
2018-05-14 18:47:06 +0200

    sc.fit_transform(y_train)
    relu relu sigmoid

--- ann_divYs.py
+++ ann_divYs.py
@@ -9,7 +9,7 @@ Created on Thu Apr 19 13:46:28 2018
 from math import ceil
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+# import pandas as pd
 
 # Importing sklearn tools for prepare data
 from sklearn.model_selection import train_test_split
@@ -38,7 +38,7 @@ def classifierAnn(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
-    # y_train = sc.fit_transform(y_train)
+    y_train = sc.fit_transform(y_train)
 
     [rowX, colX] = np.shape(X_train)
     # [row, col] = np.shape(y_train)
@@ -46,11 +46,11 @@ def classifierAnn(X, y):
     unit = ceil((colX + col)/2)
 
     classifier = Sequential()
-    classifier.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'tanh', input_dim = colX))
+    classifier.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'relu', input_dim = colX)) # relu relu sigmoid
     # classifier.add(Dropout(rate = 0.1))
-    classifier.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'tanh'))
+    classifier.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'relu'))
     # classifier.add(Dropout(rate = 0.1))
-    classifier.add(Dense(units = col, kernel_initializer = 'uniform', activation = 'linear')) #tanh linear
+    classifier.add(Dense(units = col, kernel_initializer = 'uniform', activation = 'sigmoid')) #tanh tanh linear
     classifier.compile(optimizer = 'nadam', loss = 'mean_squared_error', metrics=['mae', 'acc'])
     classifier.fit(X_train, y_train, batch_size = 27, epochs = 300)
     # Predicting the Test set results
