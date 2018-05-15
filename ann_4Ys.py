a89a281ff86b48459aba267f216be86fbacdc693
AldoContreGo <aldocontrego@gmail.com>
2018-05-15 12:50:17 +0200

    code improvements
    code improvements

--- ann_4Ys.py
+++ ann_4Ys.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 # Importing sklearn tools for prepare data
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.preprocessing import MinMaxScaler
 # Importing the Keras libraries and packages
 #import keras
 from keras.models import Sequential
@@ -27,48 +27,41 @@ def importAndPrepare(X, y):
     # Splitting the dataset into the Training set and Test set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
     # Feature Scaling
-    #sc = MinMaxScaler(feature_range = (0, 1))
-    sc = StandardScaler()
+    # sc = StandardScaler()
+    sc = MinMaxScaler(feature_range = (0.2, 0.8))
     X_train = sc.fit_transform(X_train)
     y_train = sc.fit_transform(y_train)
+    X_test = sc.fit_transform(X_test)
+    y_test = sc.fit_transform(y_test)
     # print(X_test, y_test)
     return(X, y, X_train, X_test, y_train, y_test, sc)
     #    return(X, X, y, y, sc)
 
 # Create the ANN
-def createANN(X, X_train, X_test, y_train):
+def createANN(X, X_train, X_test, y_train, y_test):
     # size of tests
     [rowX, colX] = np.shape(X_train)
     [row, col] = np.shape(y_train)
     unit = ceil((colX + col)/2)
-
+    optim, lossT = 'nadam', 'mean_squared_error'
+    batchS, nEpochs = 15, 100
     classifier = Sequential()
-    classifier.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'relu', input_dim = colX)) # relu relu sigmoid
+    classifier.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'sigmoid', input_dim = colX)) # relu relu sigmoid
     # classifier.add(Dropout(rate = 0.1))
-    classifier.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'sigmoid'))
     # classifier.add(Dropout(rate = 0.1))
-    classifier.add(Dense(units = col, kernel_initializer = 'uniform', activation = 'sigmoid')) #tanh tanh linear
-    classifier.compile(optimizer = 'nadam', loss = 'mean_squared_error', metrics=['mae', 'acc'])
-    classifier.fit(X_train, y_train, batch_size = 27, epochs = 300)
+    classifier.add(Dense(units = col, kernel_initializer = 'uniform', activation = 'linear'))      # sigmoid sigmoid linear
+    classifier.compile(optimizer = optim, loss = lossT, metrics=['mae', 'acc'])
+    classifier.fit(X_train, y_train, batch_size = batchS, epochs = nEpochs)
     # Predicting the Test set results
+    scores = classifier.evaluate(X_test, y_test)
+    print('PARAMETERS --> dense units: {0}, optimizer: {1} loss: {2} batch_size: {3} epochs: {4}'
+        .format(unit, optim, lossT, batchS, nEpochs))
+    print("{0}: {1} --> {2}: {3} %"
+        .format(classifier.metrics_names[1], scores[1], classifier.metrics_names[2], scores[2]*100))
     y_pred = classifier.predict(X)
-    # y_pred = sc.inverse_transform(y_pred)
-    # print(y_final)
     return(classifier, y_pred)
 
-# Single prediction function
-def singlePrediction(classifier, sc, y_test, y_pred):
-	# Feature Scaling
-    # TODO: singleObservation = first line of input data
-	singleObservation = np.array([[1.54, 2.08, 1.53, 1.7, 1.5, 1.14, 2.29, 1.8, 1.51, 1.81]])
-	#singleObservation = sc.transform(singleObservation)
-	new_prediction = classifier.predict(singleObservation)
-	print(new_prediction)
-	# Making the Confusion Matrix
-	# cm = confusion_matrix(y_test, y_pred)
-	# print accuracy_score(expected, y_test)
-	# print classification_report(expected, y_test)
-	# print(cm)
 
 def plotData(y_pred, y):
     fig, axs = plt.subplots(2, 2)
@@ -81,16 +74,18 @@ def plotData(y_pred, y):
     axs[0,1].set_title("Q3")
     axs[0,1].plot(y[:, 2], color = 'red', label = 'Test')
     axs[0,1].plot(y_pred[:, 2], color = 'blue', label = 'Predicted')
-    axs[1,1].set_title("Q4")
-    axs[1,1].plot(y[:, 3], color = 'red', label = 'Test')
-    axs[1,1].plot(y_pred[:, 3], color = 'blue', label = 'Predicted')
+    try:
+        axs[1,1].set_title("Q4")
+        axs[1,1].plot(y[:, 3], color = 'red', label = 'Test')
+        axs[1,1].plot(y_pred[:, 3], color = 'blue', label = 'Predicted')
+    except (IndexError):
+        pass
     plt.show()
 
 def main():
     [input, target] = usrInput.getDataset()
     X, y, X_train, X_test, y_train, y_test, sc = importAndPrepare(input, target)
-    [classifier, y_pred] = createANN(X, X_train, X_test, y_train)
-    # singlePrediction(classifier, sc, y_test, y_pred)
+    [classifier, y_pred] = createANN(X, X_train, X_test, y_train, y_test)
     plotData(y_pred, y)
 
 if __name__ == "__main__":
