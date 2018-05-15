8741a29ed74638e85224a6e173d87b458925ee64
AldoContreGo <aldocontrego@gmail.com>
2018-05-15 12:50:43 +0200

    code improvements
    code improvements

--- classifier.py
+++ classifier.py
@@ -22,17 +22,24 @@ def buildClassifierANN(X_train, y_train, X_test):
     from keras.models import Sequential
     from keras.layers import Dense
     from math import ceil
-    # Getting data information
+    # ANN parameters
     [rowX, colX] = np.shape(X_train)
     [rowy, coly] = np.shape(y_train)
     unit = ceil((colX + coly)/2)
+    optim, lossT = 'adam', 'binary_crossentropy'
+    batchS, nEpochs = 32, 100
     # Initialising the ANN
     classifierANN = Sequential()
     classifierANN.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'relu', input_dim = colX))
     classifierANN.add(Dense(units = unit, kernel_initializer = 'uniform', activation = 'relu'))
     classifierANN.add(Dense(units = coly, kernel_initializer = 'uniform', activation = 'sigmoid'))
-    classifierANN.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics=['mae', 'acc'])
-    classifierANN.fit(X_train, y_train, batch_size = 32, epochs = 100)
+    classifierANN.compile(optimizer = optim, loss = lossT, metrics=['mae', 'acc'])
+    classifierANN.fit(X_train, y_train, batch_size = batchS, epochs = nEpochs)
+    scores = classifierANN.evaluate(X_train, y_train)
+    print('PARAMETERS --> dense units: {0}, optimizer: {1} loss: {2} batch_size: {3} epochs: {4}'
+        .format(unit, optim, lossT, batchS, nEpochs))
+    print("{0}: {1} --> {2}: {3} %"
+        .format(classifierANN.metrics_names[1], scores[1], classifierANN.metrics_names[2], scores[2]*100))
     y_pred = classifierANN.predict(X_test)
     return(classifierANN, (y_pred > 0.5))
 
@@ -54,9 +61,9 @@ def main():
     [classifierANN, y_pred] = buildClassifierANN(X_train, y_train_transf, X_test)
     # transform predictions
     output_pred = decoding_inverse_transform(lb, y_pred)
-    print(output_pred)
-    # from y_test change values in csv document from "COMBO" to movement wrote down on notebook
-    print(y_test)
+    # print(output_pred)
+    # TODO: from y_test change values in csv document from "COMBO" to movement wrote down on notebook
+    # print(y_test)
 
 if __name__ == "__main__":
     main()
