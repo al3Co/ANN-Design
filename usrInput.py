a847eb21af885f390f6b43705d825ae68f852b80
al3co <aldocontrego@gmail.com>
2018-05-16 20:12:37 +0200

    code improvements

--- usrInput.py
+++ usrInput.py
@@ -6,6 +6,7 @@ Created on Thu May 10 16:05:10 2018
 """
 
 import pandas as pd
+import dataBaseClass
 
 def kindOfDataFnc():
     while True:
@@ -31,54 +32,12 @@ def kindOfTestFnc():
             print('ValueError')
     return kindOfTest
 
-def loadWSFunc(kindOfData):
-    # [0]All, [1]COMBO, [2]CRUZEXT, [3]CRUZINT, [4]ELEFRONT, [5]LATERAL, [6]ROTZ
-    if kindOfData == 1:
-        dataset = pd.read_csv('data/comboAll.csv')
-    elif kindOfData == 2:
-        dataset = pd.read_csv('data/cruzadoextAll.csv')
-    elif kindOfData == 3:
-        dataset = pd.read_csv('data/cruzadointAll.csv')
-    elif kindOfData == 4:
-        dataset = pd.read_csv('data/frontalAll.csv')
-    elif kindOfData == 5:
-        dataset = pd.read_csv('data/lateralAll.csv')
-    elif kindOfData == 6:
-        dataset = pd.read_csv('data/rotacionzAll.csv')
-    elif kindOfData == 0:
-        dataset = pd.read_csv('data/allData.csv')
-    return dataset
-
-def dataToRNN(dataset, kindOfTest):
-    # [1]FlexS vs ShoulderAng [2]FlexS+IMUq vs ShoulderAng [3]IMUq vs ShoulderAng
-    # [4]PCA vs ShoulderAng [5] FlexS vs IMUq [6] PCA vs IMUq
-    if kindOfTest == 1:     # reviewed
-        input = dataset.iloc[:, 20:35].values   # FlexS
-        target = dataset.iloc[:, 41:44].values  # ShoulderAng
-    elif kindOfTest == 2:
-        input1 = dataset.iloc[:, 20:35].values  # FlexS
-        input2 = dataset.iloc[:, 13:17].values  # IMUq
-        input = np.concatenate([input1, input2], axis=1)    # FlexS + IMUq
-        target = dataset.iloc[:, 41:44].values  # ShoulderAng
-    elif kindOfTest == 3:   # reviewed
-        input = dataset.iloc[:, 13:17].values   # IMUq
-        target = dataset.iloc[:, 41:44].values  # ShoulderAng
-    elif kindOfTest == 4:
-        input = dataset.iloc[:, 20:35].values   # TODO PCA analysis
-        target = dataset.iloc[:, 41:44].values  # ShoulderAng
-    elif kindOfTest == 5:   # reviewed
-        input = dataset.iloc[:, 20:35].values   # FlexS
-        target = dataset.iloc[:, 13:17].values  # IMUq
-    elif kindOfTest == 6:   # reviewed
-        input = dataset.iloc[:, 20:35].values   # TODO PCA analysis
-        target = dataset.iloc[:, 13:17].values   # IMUq
-    return (input, target)
-
 def getDataset():
+    db = dataBaseClass.dataBase()
     kindOfData = kindOfDataFnc()
     kindOfTest = kindOfTestFnc()
-    dataset = loadWSFunc(kindOfData)
-    [input, target] = dataToRNN(dataset, kindOfTest)
+    dataset = db.loadWSFunc(kindOfData)
+    [input, target] = db.dataToRNN(dataset, kindOfTest)
     return(input, target)
 
 if __name__ == "__main__":
