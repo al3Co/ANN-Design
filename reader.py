82904e65f46c1385e3ae6843afeac05f360ca893
al3co <aldocontrego@gmail.com>
2018-05-16 20:05:21 +0200

    code improvements

--- reader.py
+++ reader.py
@@ -1,22 +1,31 @@
 
 # method to do all ANNs & RNNs and save on a dictionary the results given
-
-
-# options
-# [0]All [1]COMBO [2]CRUZEXT [3]CRUZINT [4]ELEFRONT [5]LATERAL [6]ROTZ
-# [1]FlexS vs ShoulderAng [2]FlexS+IMUq vs ShoulderAng [3]IMUq vs ShoulderAng [4]PCA vs Shoulder [5]FlexS vs IMUq [6]PCA vs IMUq
+import csv
+import dataBaseClass
+import ann_4Ys
 
 movDict = {'all':0,'combo':1,'cruzext':2,'cruzint':3,'elefront':4,'lateral':5,'rotz':6}
 sortMov = {'FlexS vs ShoulderAng':1, 'FlexS+IMUq vs ShoulderAng':2,
             'IMUq vs ShoulderAng':3, 'PCA vs Shoulder':4,
             'FlexS vs IMUq':5, 'PCA vs IMUq':6}
-results = {}
+headers = ['Movement','Kind','classifier','loss_value-mean_absolute_error-acc','units',
+            'optimizer','loss','batch_size','epochs']
+batch_size = 32
+numEpochs  = [100, 200]
 
 def reader():
-    for mov in sortMov.keys():
-        print(mov)
-    # for mov in movDict.values():
-    #     print(mov)
+    db = dataBaseClass.dataBase()
+    with open('results.csv','w') as resF:
+        writer = csv.writer(resF, delimiter=',',lineterminator='\n',)
+        writer.writerow(headers)
+        for movK, movV in movDict.items():
+            dataset = db.loadWSFunc(movV)
+            for sortK, sortV in sortMov.items():
+                [input, target] = db.dataToRNN(dataset, sortV)
+                for nEpochs in numEpochs:
+                    [X, y, X_train, X_test, y_train, y_test, sc] = ann_4Ys.importAndPrepare(input, target)
+                    [classifier, scores, unit, optim, lossT] = ann_4Ys.createANN(X_train, X_test, y_train, y_test, batch_size, nEpochs)
+                    writer.writerow([movK, sortK, classifier, scores, unit, optim, lossT, batch_size, nEpochs])
 
 if __name__ == "__main__":
     reader()
