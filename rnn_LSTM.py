1da5ae478b3a98c3dcf34af4f44dfbd5b9584d3d
AldoContreGo <aldocontrego@gmail.com>
2018-05-17 20:51:58 +0200

    plane edition
    plane edition

--- rnn_LSTM.py
+++ rnn_LSTM.py
@@ -1,3 +1,4 @@
+# Stacked LSTM for international airline passengers problem with memory
 import numpy
 import matplotlib.pyplot as plt
 from pandas import read_csv
@@ -21,13 +22,9 @@ def create_dataset(dataset, look_back=1):
 	return numpy.array(dataX), numpy.array(dataY)
 # fix random seed for reproducibility
 numpy.random.seed(7)
-
+# load the dataset
 [input, target] = usrInput.getDataset()
 dataset = numpy.append(target, input, axis = 1)
-
-# dataframe = read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
-# dataset = dataframe.values
-# dataset = dataset.astype('float32')
 # normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
@@ -36,21 +33,27 @@ train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 # reshape into X=t and Y=t+1
-look_back = 1
+look_back = 2
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 # reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
 # create and fit the LSTM network
+print(trainX.shape)
+batch_size = 1
 model = Sequential()
-model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True, return_sequences=True))
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+for i in range(2):
+	model.fit(trainX, trainY, epochs=1, batch_size=batch_size, verbose=2, shuffle=False)
+	model.reset_states()
 # make predictions
-trainPredict = model.predict(trainX)
-testPredict = model.predict(testX)
+trainPredict = model.predict(trainX, batch_size=batch_size)
+model.reset_states()
+testPredict = model.predict(testX, batch_size=batch_size)
 # invert predictions
 trainPredict = scaler.inverse_transform(trainPredict)
 trainY = scaler.inverse_transform([trainY])
