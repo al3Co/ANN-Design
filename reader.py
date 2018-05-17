148b67fd5ab469b337fdb5934407fffb328b7bf4
al3co <aldocontrego@gmail.com>
2018-05-17 16:37:51 +0200

    RNN added

--- reader.py
+++ reader.py
@@ -2,21 +2,24 @@
 # method to do all ANNs & RNNs and save on a dictionary the results given
 import csv
 import dataBaseClass
-import ann_4Ys
 import datetime
 
 movDict = {'all':0,'combo':1,'cruzext':2,'cruzint':3,'elefront':4,'lateral':5,'rotz':6}
 sortMov = {'FlexS vs ShoulderAng':1, 'FlexS+IMUq vs ShoulderAng':2,
             'IMUq vs ShoulderAng':3, 'PCA vs Shoulder':4,
             'FlexS vs IMUq':5, 'PCA vs IMUq':6}
-headers = ['Movement','Kind','classifier','units','optimizer','loss',
+headers = ['Movement','Kind','Sequential','units','optimizer','loss',
             'batch_size','epochs','loss_value-mean_absolute_error-acc']
 batch_size = 32
-numEpochs  = [100, 200]
+numEpochs  = [200, 500]
 
-def readerANN():
+options = {'ANN':0, 'RNN':1}
+
+def reader(optKey, optVal):
+    if optVal == 0: import ann_4Ys
+    else: import rnn_4Ys
     db = dataBaseClass.dataBase()
-    with open(('results/resultsANN'+str(datetime.datetime.now())+'.csv'),'w') as resF:
+    with open(('results/results'+ optKey + str(datetime.datetime.now())+'.csv'),'w') as resF:
         writer = csv.writer(resF, delimiter=',',lineterminator='\n',)
         writer.writerow(headers)
         for movK, movV in movDict.items():
@@ -24,14 +27,12 @@ def readerANN():
             for sortK, sortV in sortMov.items():
                 [input, target] = db.dataToRNN(dataset, sortV)
                 for nEpochs in numEpochs:
-                    [X, y, X_train, X_test, y_train, y_test, sc] = ann_4Ys.importAndPrepare(input, target)
-                    [classifier, scores, unit, optim, lossT] = ann_4Ys.createANN(X_train, X_test, y_train, y_test, batch_size, nEpochs)
+                    if optVal == 0:
+                        [X, y, X_train, X_test, y_train, y_test, sc] = ann_4Ys.importAndPrepare(input, target)
+                        [classifier, scores, unit, optim, lossT] = ann_4Ys.createANN(X_train, X_test, y_train, y_test, batch_size, nEpochs)
+                    else: [classifier, scores, unit, optim, lossT] = rnn_4Ys.createRNN(input, target, batch_size, nEpochs)
                     writer.writerow([movK, sortK, classifier, unit, optim, lossT, batch_size, nEpochs, scores])
 
-def readerRNN():
-    db = dataBaseClass.dataBase()
-
-
-
 if __name__ == "__main__":
-    reader()
+    for k, v in options.items():
+        reader(k,v)
