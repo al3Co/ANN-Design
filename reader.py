aac84df5d4a730cd6bb2ec84e7838fdad086891a
al3co <aldocontrego@gmail.com>
2018-05-18 11:17:50 +0200

    comments added

--- reader.py
+++ reader.py
@@ -1,20 +1,23 @@
 
-# method to do all ANNs & RNNs and save on a dictionary the results given
+# method to run ANNs and RNNs and save the results given in the csv file
 import csv
 import dataBaseClass
 import datetime
 
+# specific parameters of the phenomenon
 movDict = {'all':0,'combo':1,'cruzext':2,'cruzint':3,'elefront':4,'lateral':5,'rotz':6}
 sortMov = {'FlexS vs ShoulderAng':1, 'FlexS+IMUq vs ShoulderAng':2,
             'IMUq vs ShoulderAng':3, 'PCA vs Shoulder':4,
             'FlexS vs IMUq':5, 'PCA vs IMUq':6}
 headers = ['Movement','Kind','Sequential','units','optimizer','loss',
             'batch_size','epochs','loss_value-mean_absolute_error-acc']
+
+# DL parameters
 batch_size = [10, 20, 32]
 numEpochs  = [300, 400]
-
 options = {'ANN':0, 'RNN':1}
 
+# function to run DL using dataset from data floder
 def reader(optKey, optVal):
     if optVal == 0: import ann_4Ys
     else: import rnn_4Ys
@@ -34,7 +37,7 @@ def reader(optKey, optVal):
                         else: [classifier, scores, unit, optim, lossT] = rnn_4Ys.createRNN(input, target, batch, nEpochs)
                         writer.writerow([movK, sortK, classifier, unit, optim, lossT, batch, nEpochs, scores])
                         print('Global Progress: ', optKey, batch, nEpochs, movK, sortK)
-
+# main
 if __name__ == "__main__":
     for k, v in options.items():
         reader(k,v)
