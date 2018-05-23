4c08f4aa6ac5e1ea7daea9403bf4c99febe3e3b5
al3co <aldocontrego@gmail.com>
2018-05-23 19:49:14 +0200

    update tests

--- tests.py
+++ tests.py
@@ -1,8 +1,34 @@
 
-
+# method to run ANNs and RNNs and save the results given in the csv file
+import time
+import csv
 import dataBaseClass
+import datetime
 
-db = dataBaseClass.dataBase()
-dataset = db.loadWSFunc(6)
-[input, target] = db.dataToRNN(dataset, 4)
-print(input)
+# specific parameters of the phenomenon
+movDict = {'all':0,'combo':1,'cruzext':2,'cruzint':3,'elefront':4,'lateral':5,'rotz':6}
+sortMov = {'FlexS vs ShoulderAng':1, 'FlexS+IMUq vs ShoulderAng':2,
+            'IMUq vs ShoulderAng':3, 'PCA vs Shoulder':4,
+            'FlexS vs IMUq':5, 'PCA vs IMUq':6}
+headers = ['Movement','Kind','Sequential','units','optimizer','loss',
+            'batch_size','epochs','loss_value-mean_absolute_error-acc', 'Time']
+option = 'reader'
+# DL parameters
+batch_size = [10, 20, 32]   # 10 20 32
+numEpochs  = [300, 400]     # 300 400
+optim_lossT = ['nadam', 'mean_squared_error']
+activ = ['sigmoid', 'linear']
+network = {'ANN':0, 'RNN':1}
+
+# function to run DL using dataset from data floder
+def reader(optKey, optVal):
+    for movK, movV in movDict.items():
+        for sortK, sortV in sortMov.items():
+            for nEpochs in numEpochs:
+                for batch in batch_size:
+                    print('Global Progress: ', optKey, batch, nEpochs, movK, sortK)
+
+# main
+if __name__ == "__main__":
+    for k, v in network.items():
+        reader(k,v)
